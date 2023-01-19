89f60525d4c13b0940f0a34ff0434382f68a69be
KaalaAadmi <arnav264@gmail.com>
2023-01-19 22:00:03 +0000

    Map Marker Data Added

---
+++ src/assets/data.js
@@ -0,0 +1,40 @@
+export const markerData = [
+  {
+    latitude: 53.347785,
+    longitude: -6.274716,
+    description: "Report 1",
+  },
+  {
+    latitude: 53.355162,
+    longitude: -6.259257,
+    description: "Report 2",
+  },
+  {
+    latitude: 53.34594,
+    longitude: -6.244827,
+    description: "Report 3",
+  },
+  {
+    latitude: 53.338971,
+    longitude: -6.248606,
+    description: "Report 4",
+  },
+  {
+    latitude: 53.3418,
+    longitude: -6.263104,
+    description: "Report 5",
+  },
+];
+
+export const disasterData = [
+  {
+    latitude: 53.34425,
+    longitude: -6.26241,
+    description: "Disaster 1",
+  },
+  {
+    latitude: 53.330456207499424,
+    longitude: -6.278736499782899,
+    description: "Disaster 2",
+  },
+];
