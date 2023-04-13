be34a385073a004e38567946fbb4e3da24558585
Vivek Bhadula <vivekbhadula201420@gmail.com>
2023-04-13 17:51:49 +0100

    changed login

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 // import styled from "styled-components";
+import axios from 'axios'
+
 import "./LoginScreen.css";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async()=>{
+    const res= await axios.post('url',{email,password})
+    console.log(res.data)
+  }
+
   return (
     <div className="container">
       <div className="loginContainer">
         <div className="input-group">
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
           <label className="placeholder">Email</label>
         </div>
         <div className="input-group">
-          <input type="text" className="input" />
+          <input type="text" className="input" value={password} onChange={(event)=>{setPassword(event.target.value)}} />
           <label className="placeholder">Password</label>
         </div>
         {/* <div className="submitContainer"> */}
-        <button className="btnSubmit" type="submit">
+        <button className="btnSubmit" type="submit" onClick={handleSubmit}>
           LOGIN
         </button>
         {/* </div> */}
