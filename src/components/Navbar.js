7bfba9419c733f417bad4c5c43ad115634aec7ba
KaalaAadmi <arnav264@gmail.com>
2023-04-14 18:10:36 +0100

    Refactored the routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,15 @@ function Navbar() {
 
 	return (
 		<header>
-			<h3>LOGO</h3>
+			<h3>DISASTRO</h3>
 			<nav ref={navRef}>
 				<a href="/#" className="active">Home</a>
 				<a href="/login">Login</a>
 				<a href="/maps">Map</a>
-				<a href="/bulletin">Bulletin</a>
 				<a href="/faq">FAQ</a>
 				<a href="/report-disaster">Report Disaster</a>
 				<a href="/view-reports">View Reports</a>
 				<a href="/message-hq">Message HQ</a>
-				<a href="/disaster-information">Disaster Information</a>
 				<a href="/send-resources">Send Resources</a>
 				<a href="/create-disaster">Create Disaster</a>
 				<button
